72cf4801d9ae5183ebe1fc9a66734436f10840bf
Killian Courvoisier <dev.killian.courvoisier@gmail.com>
2021-09-06 17:23:06 +0200

    controlled input on form, newMaze button not handled yet

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,47 @@
 // == Import npm
-import React from 'react';
+import React, { useState } from 'react';
 
 // == Import
 import './styles.css';
 
 import Mazeboard from 'src/components/Mazeboard/mazeboard';
+import Form from 'src/components/Form/form';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <Mazeboard />
-  </div>
-);
+const App = () => {
+  const [horizontalValue, setHorizontalValue] = useState(8);
+  const [verticalValue, setVerticalValue] = useState(8);
+
+  const fetchData = () => {
+    console.log(typedNumber);
+  };
+
+  const handleHorizontalInputChange = (typedNumber) => {
+    setHorizontalValue(typedNumber);
+  };
+
+  const handleVerticalInputChange = (typedNumber) => {
+    setVerticalValue(typedNumber);
+  };
+
+  const handleFormSubmit = () => {
+    fetchData();
+  };
+
+  return (
+
+    <div className="app">
+      <Form
+        onFormSubmit={handleFormSubmit}
+        onHorizontalInputChange={handleHorizontalInputChange}
+        onVerticalInputChange={handleVerticalInputChange}
+        horizontalValue={horizontalValue}
+        verticalValue={verticalValue}
+      />
+      <Mazeboard horizontalValue={horizontalValue} verticalValue={verticalValue} />
+    </div>
+  );
+};
 
 // == Export
 export default App;
---
+++ src/components/Form/form.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+const Form = ({
+  onFormSubmit,
+  onHorizontalInputChange,
+  onVerticalInputChange,
+  horizontalValue,
+  verticalValue,
+}) => (
+  <form
+    className="form"
+    onSubmit={(evt) => {
+      evt.preventDefault();
+      onFormSubmit();
+    }}
+  >
+    <div className="form__input">
+      <label htmlFor="verticalAxis">
+        Vertical Axis
+        <input
+          id="verticalAxis"
+          type="number"
+          placeholder={verticalValue}
+          onChange={(evt) => {
+            const number = parseInt(evt.target.value, 10);
+            onVerticalInputChange(number);
+          }}
+        />
+      </label>
+
+      <label htmlFor="horizontalAxis">
+        Horizontal Axis
+        <input
+          id="horizontalAxis"
+          type="number"
+          placeholder={horizontalValue}
+          onChange={(evt) => {
+            const number = parseInt(evt.target.value, 10);
+            onHorizontalInputChange(number);
+          }}
+        />
+      </label>
+    </div>
+    <div className="form__submit">
+      <input id="button__submit" type="submit" value="New Maze" />
+    </div>
+  </form>
+);
+
+Form.propTypes = {
+  onFormSubmit: PropTypes.func.isRequired,
+  onHorizontalInputChange: PropTypes.func.isRequired,
+  onVerticalInputChange: PropTypes.func.isRequired,
+  horizontalValue: PropTypes.number.isRequired,
+  verticalValue: PropTypes.number.isRequired,
+};
+export default Form;
